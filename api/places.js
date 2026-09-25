a43670c8922ecd8ee3a6792683340cbd3847848a
Emanuel Martinez <emartinezra112@icloud.com>
2026-09-24 22:06:22 -0600

    Route business review API requests on Vercel

--- api/places.js
+++ api/places.js
@@ -1,3 +1,13 @@
 import app from '../server.js';
 
-export default app;
+export default function placesHandler(req, res) {
+  const requestUrl = new URL(req.url || '/', `https://${req.headers.host || 'localhost'}`);
+  const reviewPlaceId = requestUrl.searchParams.get('__reviewPlaceId');
+
+  if (reviewPlaceId) {
+    requestUrl.searchParams.delete('__reviewPlaceId');
+    req.url = `/api/places/${encodeURIComponent(reviewPlaceId)}/reviews${requestUrl.search}`;
+  }
+
+  return app(req, res);
+}
